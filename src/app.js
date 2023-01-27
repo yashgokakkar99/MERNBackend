44f53e47239373ecd5e72045ad9d0ff56d026c20
Yash Gokakkar <yashgokakkar@gmail.com>
2023-01-27 10:08:53 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = process.env.PORT || 3000
 
+// Database connectivity
+require("./db/connect")
+
+app.use(express.static(path.join(__dirname, '../public')))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Main Server Connected.......')
 })
 
+// app.get('/index',(req,res) => {
+//     res.send(path.join())
+// })
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server listening on port : ${port}`)
 })
---
+++ src/db/connect.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/Comapny')
+.then(()=>{
+    console.log(`Database connection successfull !!`)
+})
+.catch((e)=>{
+    console.log(`Database not connected :(`)
+})
